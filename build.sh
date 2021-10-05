40b54c001d3924ede929d2774faabcbc2dd7ef81
Alex Crichton <alex@alexcrichton.com>
2021-10-05 08:18:47 -0700

    Add bare-bones host

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+set -ex
 cargo build --manifest-path wasm/Cargo.toml --target wasm32-wasi --release
+(cd host && npx tsc host.ts)
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::SystemTime;
+
+witx_bindgen_rust::import!("../imports.witx");
+witx_bindgen_rust::export!("../exports.witx");
+
+struct Exports;
+
+#[witx_bindgen_rust::async_trait(?Send)]
+impl exports::Exports for Exports {
+    async fn run() {
+        println!("current time is {:?}", SystemTime::now());
+        // ...
+    }
 }
